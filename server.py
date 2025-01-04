21ca11d39b0f9119bb0fbdb9bf72787c2cc182a6
UpByTheStars <upbythestars@gmail.com>
2025-01-03 23:04:02 -0500

    https support

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse, FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 import ssl, os
+import uvicorn
 
 app = FastAPI()
 
@@ -40,6 +41,10 @@ app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 
 if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=6789)
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=6789,
+        ssl_keyfile="keyfile.key",
+        ssl_certfile="certfile.crt",
+    )
